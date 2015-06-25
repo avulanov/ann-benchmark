4a2a843ea446fed443f7ac85887d2bfe8c2b77ed
Alexander Ulanov <nashb@yandex.ru>
2015-06-25 14:32:06 -0700

    caffe configurations and spark code snippet

---
+++ spark/spark.scala
@@ -0,0 +1,19 @@
+import org.apache.log4j._
+Logger.getRootLogger.setLevel(Level.OFF)
+import org.apache.spark.mllib.ann.{FeedForwardTrainer, FeedForwardTopology}
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.mllib.classification.ANNClassifier
+val train = MLUtils.loadLibSVMFile(sc, "hdfs://mercado-9.hpl.hp.com:9000/user/hduser/input/mnist8m.scale").persist
+train.count
+
+// huge topology
+val train = MLUtils.loadLibSVMFile(sc, "hdfs://mercado-9.hpl.hp.com:9000/user/hduser/input/mnist.scale")
+val train5 = train.repartition(5)
+train5.persist
+train5.count
+val topology = FeedForwardTopology.multiLayerPerceptron(Array[Int](780, 2500, 2000, 1500, 1000, 500, 10), false)
+val trainer = new FeedForwardTrainer(topology, 780, 10).setBatchSize(1000)
+trainer.SGDOptimizer.setNumIterations(2).setMiniBatchFraction(1.0).setStepSize(0.03)
+val model = new ANNClassifier(trainer).train(train5)
