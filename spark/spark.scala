4404ff664a661e301bee09b4a777226fcda934fc
Alexander Ulanov <nashb@yandex.ru>
2015-07-06 12:23:18 -0700

    spark measurement: loop

--- spark/spark.scala
+++ spark/spark.scala
@@ -5,12 +5,24 @@ import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.util.MLUtils
 import org.apache.spark.mllib.classification.ANNClassifier
-// huge topology
-val train = MLUtils.loadLibSVMFile(sc, "hdfs://my.server:9000/data/mnist.scale")
-val train5 = train.repartition(5)
-train5.persist
-train5.count
+// maximum number of worker nodes in cluster
+val numNodes = 5
+// batch size, ~10K is good for GPU
+val batchSize = 10000
+// number of iterations to run
+val numIterations = 5
+val train = MLUtils.loadLibSVMFile(sc, "/mnist.scale")
 val topology = FeedForwardTopology.multiLayerPerceptron(Array[Int](780, 2500, 2000, 1500, 1000, 500, 10), false)
-val trainer = new FeedForwardTrainer(topology, 780, 10).setBatchSize(1000)
-trainer.SGDOptimizer.setNumIterations(2).setMiniBatchFraction(1.0).setStepSize(0.03)
-val model = new ANNClassifier(trainer).train(train5)
+val trainer = new FeedForwardTrainer(topology, 780, 10).setBatchSize(batchSize)
+trainer.SGDOptimizer.setNumIterations(numIterations).setMiniBatchFraction(1.0).setStepSize(0.03)
+// parallalize the data for N nodes, persist, run X iterations and print average time for each run
+for (i <- 1 to numNodes) {
+	val dataPartitions = sc.parallelize(1 to i, i)
+	val sample = train.sample(true, 1.0 / i, 11L).collect
+	val parallelData = dataPartitions.flatMap(x => sample)
+	parallelData.persist
+	parallelData.count
+	val t = System.nanoTime()
+	val model = new ANNClassifier(trainer).train(parallelData)
+	println(i + "\t" + batchSize + "\t" + (System.nanoTime() - t) / (numIterations * 1e9)) 	
+}
